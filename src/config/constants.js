4b44437a21cca751725ba529873377f57f9456ea
Carlos Henrique <hencarlosdv@gmail.com>
2025-06-04 10:58:21 -0300

    test ci 2

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,12 +3,4 @@ module.exports = {
   MAX_TITLE_LENGTH: 70,
   GEMINI_API_URL: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent',
   
-  
-  MESSAGES: {
-    TOKENS_REQUIRED: 'Os tokens do GitHub e Gemini são obrigatórios.',
-    PR_ONLY: 'This action can only be run on pull requests.',
-    ACTION_COMPLETED: 'Ação concluída.',
-    GEMINI_SUCCESS: 'Resposta da API Gemini recebida com sucesso.',
-    INVALID_GEMINI_RESPONSE: 'Resposta inválida da API Gemini.'
-  }
 };
